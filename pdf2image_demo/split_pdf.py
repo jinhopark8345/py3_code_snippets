14b93dd529fb1362fc0dc95fa0ea8f50fbc52f13
Jinho <jinhopark8345@gmail.com>
2022-06-12 22:48:22 +0900

    rename pdf2image folder

---
+++ pdf2image_demo/split_pdf.py
@@ -0,0 +1,52 @@
+import os
+import sh
+from pdf2image import convert_from_path
+from absl import app, flags, logging
+
+flags.DEFINE_string("src", "", "src pdf path")
+flags.DEFINE_string("dst", "", "output image dir")
+flags.DEFINE_string("fmt", "png", "output image format")
+flags.DEFINE_integer("dpi", 600, "dots per inch")
+
+FLAGS = flags.FLAGS
+
+""" need poppler(pdf rendering library) and pdf2image package (python) """
+
+def split_pdf_to_images(
+    src_pdf_path: str,
+    output_image_dir: str,
+    output_image_format: str = "png",
+    dpi: int = 600,
+) -> None:
+    """split pdf into images
+    :param src_pdf_path: source pdf path
+    :param output_image_dir: output image directory
+    :param output_image_format: output image format
+    :param dpi: output image dpi
+    :returns: None
+
+    """
+    # make sure the dst folder exist
+    os.makedirs(output_image_dir, exist_ok=True)
+
+    # split pdf to pages,
+    pages = convert_from_path(src_pdf_path, dpi=dpi, fmt=output_image_format)
+
+    # get file_path without parent directory path
+    basename = os.path.basename(src_pdf_path)
+
+    # new file path with output_image_dir
+    file_path = os.path.join(output_image_dir, basename)
+
+    for idx, page in enumerate(pages):
+        output_image_path = f"{file_path}_{idx}.{output_image_format.lower()}"
+        logging.info(f"save splited image on {output_image_path}")
+        page.save(output_image_path, output_image_format)
+
+
+def main(_):
+    split_pdf_to_images(FLAGS.src, FLAGS.dst, FLAGS.fmt, FLAGS.dpi)
+
+
+if __name__ == "__main__":
+    app.run(main)
