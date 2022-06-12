0589844a071a43259c1044c366ac9ea98e79c8f9
Jinho <jinhopark8345@gmail.com>
2022-06-12 22:35:44 +0900

    add demo to main

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ from pdf2image_usage.pdf_utils import split_pdf_to_images
 import os
 
 
-
-
-# from config.definitions import ROOT_DIR
-# print(os.path.join(ROOT_DIR, 'data', 'mydata.json'))
-
-# root = os.path.relpath(os.path.join(os.getcwd(), "data"))
-root = os.path.realpath('..')
-print(root)
-#
-split_pdf_to_images("./pdf2image_usage/data/attenion.pdf", "./pdf2image_usage/data/output3", dpi= 200)
+def demo_split_pdf_to_images():
+    root = os.path.realpath('.')
+    src_path = os.path.join(root, "pdf2image_usage/data/src/lottery.pdf")
+    dst_path = os.path.join(root, "pdf2image_usage/data/dst/")
+    split_pdf_to_images(src_path, dst_path, dpi= 200)
