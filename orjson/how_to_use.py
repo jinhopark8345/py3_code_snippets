9bbcc2424eaabd47bb25f3f33ae5d5a6b7ba8b2d
jinho <jinhoparkseoul@gmail.com>
2023-07-29 17:52:05 +0900

    update

--- orjson/how_to_use.py
+++ orjson/how_to_use.py
@@ -1,5 +1,7 @@
 
 import orjson
 
-with open(json_path, 'r') as f:
-    data = orjson.loads(f.read())
+
+def how_to_load_with_orjson():
+    with open(json_path, 'r') as f:
+        data = orjson.loads(f.read())
